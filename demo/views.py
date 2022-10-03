44e447a51bf560866868ff8b16a5a93ceeb8bce4
Alamin Musa Magaga <55305080+alaminmagaga@users.noreply.github.com>
2022-10-03 11:45:30 -0700

    Update views.py

--- demo/views.py
+++ demo/views.py
@@ -26,20 +26,38 @@ def index(request):
             response += "1. Plateau \n"
             response += "2. Ekiti \n"
             response += "3. Edo \n"
-               
+            
         elif text=="1*1":
+            response = "CON Select LGE \n"
+            response += "1. Barkin Ladi \n"
+            response += "2. Bassa \n"
+            response += "3. Bokkos \n"
+            response += "1. Jos-East \n"
+            response += "2. Jos-North \n"
+            response += "3. Jos-South \n"
+            response += "1. Kanam \n"
+            response += "2. Kanke \n"
+            response += "3. Langtang North \n"
+            response += "1. Langtang South \n"
+          
+               
+        elif text=="1*1*1":
             response = "CON Select MDA \n"
             response += "1. Ministry of Works \n"
             response += "2. Ministry of Health \n"
             response += "3. Ministry of Water Resources and Energy \n"
             response += "4. Ministry of Tourism, Culture and Hospitality \n"
             
-        elif text == "1*1*1":
-            response = "CON Contractor \n"
-            response += "1.  P.W Nig Limited \n"
+        elif text == "1*1*1*1":
+            response = "CON Select Contractor \n"
             response += "2.  Fundation solid Nigeria \n"
+            response += "1.  P.W Nig Limited \n"
             response += "3.  Rick Rock Construction Nig. Ltd. \n"
             response += "3.  EEC. International Co. Ltd \n"
+            
+        elif text == "1*1*1*: 
+            response = "END Dualization of Polo Roundabout-Farin Gada Algadama-Rock Haven Alheri Road Network and Terminus-Bauchi Road at JOS/NORTH, 100 \n"  
+       
 
        
 
